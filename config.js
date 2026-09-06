4c09c94269920e7762e68a077c38f2205ad98662
Archon <zrfoxtrot@gmail.com>
2026-09-06 16:20:19 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Public website configuration. No secrets belong in this file. */
 window.RD_CONFIG = {
   departmentName: "RESEARCH DEPARTMENT",
-  sheetId: "PASTE_YOUR_PUBLISHED_GOOGLE_SHEET_ID_HERE",
+  sheetId: "1QrQgooQIqCJ32EkWffdwSoV7ER3Q0F-fpy_6EeHciGM",
   tabs: { announcements: "Announcements", tests: "Test Logs", scps: "SCPs", roster: "Roster", ranks: "Ranks" }
 };
